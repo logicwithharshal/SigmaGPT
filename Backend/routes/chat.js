8b4a1b7e190e20c9b84146fd4603f82369600421
Harshal Raut <rharshal398@gmail.com>
2026-07-29 23:42:55 +0530

    Complete Backend

--- Backend/routes/chat.js
+++ Backend/routes/chat.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Thread from "../models/Thread.js";
+import getOpenAIAPIResponce from "../utils/openai.js";
 
 const router = express.Router();
 
@@ -56,6 +57,39 @@ router.delete("/thread/:threadId", async(req,res)=>{
     }catch(err){
         console.log(err);
         res.status(500).json({error: "Failed to Delete Thread"});
+    }
+});
+
+router.post("/chat", async(req, res)=>{
+    const {threadId, message} = req.body;
+
+    if(!threadId || !message){
+        res.status(404).json({error: "Missing required fields"})
+    }
+
+    try{
+        const thread = await Thread.findOne({threadId});
+
+        if(!thread){
+            thread = new Thread({
+                threadId,
+                title: message,
+                message: [{roles: "user", content: message}]
+            });
+        }else{
+            thread.messages.push({role: "user", content: message});
+        }
+        const  assistantReply = await getOpenAIAPIResponce(message);
+        
+        thread.messages.push({role: "assistant", content: assistantReply});
+        thread.updateAt = new Date();
+
+        await thread.save();
+        res.json({reply: assistantReply});
+
+    }catch(err){
+        console.log(err);
+        res.status(500).json({errot: "Something went Wrong"});
     }
 });
 
