91c968fe3b5077c6b9ad3acb302c0aec685be66b
Harshal Raut <rharshal398@gmail.com>
2026-08-05 00:03:05 +0530

    new for Frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -11,13 +11,15 @@ function App() {
   const [currThreadId, setCurrThreadId] = useState(uuidv1());
   const [prevChats, setPrevChats] = useState([]);
   const [newChat, setNewChat] = useState(true);
+  const [allThreads, setAllThreads] = useState([]);
 
   const providerValue = {
     prompt, setPrompt,
     reply, setReply,
     currThreadId, setCurrThreadId,
     newChat, setNewChat,
-    prevChats, setPrevChats
+    prevChats, setPrevChats,
+    allThreads, setAllThreads
   };
 
   return (
--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,6 +1,28 @@
 import "./Sidebar.css";
+import { useContext, useEffect } from "react";
+import { MyContext } from "./MyContext.jsx";
 
 function Sidebar(){
+
+    const {allThreads, setAllThreads, currThreadId} = useContext(MyContext);
+
+    const getAllThreads = async() => {
+        try{
+            const respoce = await fetch("http://localhost:8080/api/thread");
+            const res = await respoce.json();
+            const filteredData = res.map(thread => ({threadId: thread.threadId, title: thread.title}));
+            console.log(filteredData);
+            setAllThreads(filteredData);
+        }catch(err){
+            console.log(err);
+        }
+    };
+
+    useEffect(()=> {
+        getAllThreads();
+    }, [])
+
+
     return(
         <section className="sidebar">
             {/* new chat button */}
@@ -11,10 +33,11 @@ function Sidebar(){
 
             {/* histort */}
             <ul className="history">
-                <li>History1</li>
-                <li>History2</li>
-                <li>History3</li>
-                <li>History4</li>
+                {
+                    allThreads?.map((thread, idx) => (
+                        <li key={idx}>{thread.title}</li>
+                    ))
+                }
             </ul>
 
             {/* Sign */}
